6f462532f8a3111277a09ffe32c456e80e656ebf
Kshitij13579 <kshitijyerande@gmail.com>
2023-07-03 23:09:47 -0400

    feature: manage bill feature : manage user

--- cafe-springboot-angular-frontend/Frontend/src/app/material-component/material.routing.ts
+++ cafe-springboot-angular-frontend/Frontend/src/app/material-component/material.routing.ts
@@ -4,6 +4,8 @@ import { ManageCategoryComponent } from './manage-category/manage-category.compo
 import { RouteGuardService } from '../services/route-guard.service';
 import { ManageProductComponent } from './manage-product/manage-product.component';
 import { ManageOrderComponent } from './manage-order/manage-order.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
+import { ManageUserComponent } from './manage-user/manage-user.component';
 
 
 export const MaterialRoutes: Routes = [
@@ -30,5 +32,22 @@ export const MaterialRoutes: Routes = [
         data:{
             expectedRole:['admin','user']
          }
+    },
+    {
+        path:'bill',
+        component:ViewBillComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            expectedRole:['admin','user']
+         }
+    },
+    {
+        path:'user',
+        component:ManageUserComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            expectedRole:['admin']
+         }
     }
+
 ];
--- cafe-springboot-angular-frontend/Frontend/src/app/services/bill.service.ts
+++ cafe-springboot-angular-frontend/Frontend/src/app/services/bill.service.ts
@@ -26,4 +26,11 @@ export class BillService {
     return this.httpClient.get(this.url+"/bill/getBills");
   }
 
+  delete(id:any){
+    return this.httpClient.post(this.url+
+      "/bill/delete/"+id,{
+        headers:new HttpHeaders().set('Content-Type',"application/json")
+      });
+  }
+
 }
